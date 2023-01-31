a266c11807394c658d377962e2e4374cb034e660
Md. Jafrul Hasan Rasel <73773576+jhrasel@users.noreply.github.com>
2023-01-31 19:08:00 +0600

    Done Namta

--- frontend_assets/js/custome.js
+++ frontend_assets/js/custome.js
@@ -1,11 +1,21 @@
 let input   = document.querySelector("input");
+
 let button  = document.querySelector("button");
 
 
 button.addEventListener("click", function (){
 
-    for(i = 1; i <= 10; i++) {
-        document.write( input.value + " x " + i +  " = " + input.value * i + " </br> " )
-    } 
+    for(i=1; i<=10; i++){
+
+        document.write(i + " x " + input.value + " = " + input.value*i + '</br>')
+
+    }
 
 })
+
+/*
+
+2*1 = 2
+2*2 = 4
+
+*/
